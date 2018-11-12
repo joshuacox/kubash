fc7e2517ab1fe3e274ab4f6ec46600dd67fcd35d
Josh Cox <josh@webhosting.coop>
2018-11-12 09:18:46 -0600

    admin.conf

--- templates/kube_stacked_init.sh
+++ templates/kube_stacked_init.sh
@@ -17,3 +17,7 @@ systemctl restart kubelet
 echo "kubectl exec -n kube-system etcd-${CP0_HOSTNAME} -- etcdctl --ca-file /etc/kubernetes/pki/etcd/ca.crt --cert-file /etc/kubernetes/pki/etcd/peer.crt --key-file /etc/kubernetes/pki/etcd/peer.key --endpoints=https://${CP0_IP}:2379 member add ${CP1_HOSTNAME} https://${CP1_IP}:2380"
 kubectl exec -n kube-system etcd-${CP0_HOSTNAME} -- etcdctl --ca-file /etc/kubernetes/pki/etcd/ca.crt --cert-file /etc/kubernetes/pki/etcd/peer.crt --key-file /etc/kubernetes/pki/etcd/peer.key --endpoints=https://${CP0_IP}:2379 member add ${CP1_HOSTNAME} https://${CP1_IP}:2380
 kubeadm alpha phase etcd local --config /etc/kubernetes/kubeadmcfg.yaml
+kubeadm alpha phase kubeconfig all --config /etc/kubernetes/kubeadmcfg.yaml
+kubeadm alpha phase controlplane all --config  /etc/kubernetes/kubeadmcfg.yaml
+kubeadm alpha phase kubelet config annotate-cri --config /etc/kubernetes/kubeadmcfg.yaml
+kubeadm alpha phase mark-master --config /etc/kubernetes/kubeadmcfg.yaml
