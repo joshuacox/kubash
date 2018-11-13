3de0b82760b954507c57c7d46e4fa2c5837296a9
Josh Cox <josh@webhosting.coop>
2018-11-12 21:57:10 -0600

    kubespray-cluster added

--- templates/kube_stacked_init.sh
+++ templates/kube_stacked_init.sh
@@ -8,17 +8,17 @@ export CP0_HOSTNAME=$2
 export CP1_IP=$3
 export CP1_HOSTNAME=$4
 export KUBECONFIG=/etc/kubernetes/admin.conf
-mkdir -p ~/.kube/config
-cp /etc/kubernetes/admin.conf ~/.kube/config
+mkdir -p ~/.kube
+cp -v /etc/kubernetes/admin.conf ~/.kube/config
 
 kubeadm alpha phase certs all --config /etc/kubernetes/kubeadmcfg.yaml
 kubeadm alpha phase kubelet config write-to-disk --config /etc/kubernetes/kubeadmcfg.yaml
 kubeadm alpha phase kubelet write-env-file --config /etc/kubernetes/kubeadmcfg.yaml
 kubeadm alpha phase kubeconfig kubelet --config /etc/kubernetes/kubeadmcfg.yaml
 systemctl restart kubelet
-echo "kubectl exec -n kube-system etcd-${CP0_HOSTNAME} -- etcdctl --ca-file /etc/kubernetes/pki/etcd/ca.crt --cert-file /etc/kubernetes/pki/etcd/peer.crt --key-file /etc/kubernetes/pki/etcd/peer.key --endpoints=https://${CP0_IP}:2379 member add ${CP1_HOSTNAME} https://${CP1_IP}:2380"
-sleep 16
-kubectl exec -n kube-system etcd-${CP0_HOSTNAME} -- etcdctl --ca-file /etc/kubernetes/pki/etcd/ca.crt --cert-file /etc/kubernetes/pki/etcd/peer.crt --key-file /etc/kubernetes/pki/etcd/peer.key --endpoints=https://${CP0_IP}:2379 member add ${CP1_HOSTNAME} https://${CP1_IP}:2380
+echo "kubectl --kubeconfig=/etc/kubernetes/admin.conf exec -n kube-system etcd-${CP0_HOSTNAME} -- etcdctl --ca-file /etc/kubernetes/pki/etcd/ca.crt --cert-file /etc/kubernetes/pki/etcd/peer.crt --key-file /etc/kubernetes/pki/etcd/peer.key --endpoints=https://${CP0_IP}:2379 member add ${CP1_HOSTNAME} https://${CP1_IP}:2380"
+sleep 66
+kubectl --kubeconfig=/etc/kubernetes/admin.conf exec -n kube-system etcd-${CP0_HOSTNAME} -- etcdctl --ca-file /etc/kubernetes/pki/etcd/ca.crt --cert-file /etc/kubernetes/pki/etcd/peer.crt --key-file /etc/kubernetes/pki/etcd/peer.key --endpoints=https://${CP0_IP}:2379 member add ${CP1_HOSTNAME} https://${CP1_IP}:2380
 kubeadm alpha phase etcd local --config /etc/kubernetes/kubeadmcfg.yaml
 kubeadm alpha phase kubeconfig all --config /etc/kubernetes/kubeadmcfg.yaml
 kubeadm alpha phase controlplane all --config  /etc/kubernetes/kubeadmcfg.yaml
