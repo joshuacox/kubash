904ef1cd64e013455ee62c17a7e28db409c793f0
Josh Cox <josh@webhosting.coop>
2019-04-22 10:21:50 -0500

    Adding certmanager to istio install

--- lib/kattic.bash
+++ lib/kattic.bash
@@ -566,9 +566,21 @@ do_metallb () {
 }
 
 do_istio () {
-    cd $KUBASH_DIR/submodules/istio/install
-    cd kubernetes/helm
-    kubectl --kubeconfig=$KUBECONFIG apply -f \
+    # Install istio with certmanager
+    # https://istio.io/docs/examples/advanced-gateways/ingress-certmgr/
+    if [ -z $LETSENCRYPT_EMAIL ]; then
+      echo "Type the email that you want to use for lets encrypt, followed by [ENTER]:"
+      read LETSENCRYPT_EMAIL
+    fi
+    cd $KUBASH_DIR/submodules/istio/install/kubernetes/helm
+    helm install \
+      --namespace=istio-system \
+      --set gateways.istio-ingressgateway.sds.enabled=true \
+      --set global.k8sIngress.enabled=true \
+      --set global.k8sIngress.enableHttps=true \
+      --set global.k8sIngress.gatewayName=ingressgateway \
+      --set certmanager.enabled=true \
+      --set certmanager.email=$LETSENCRYPT_EMAIL \
       istio
 }
 
