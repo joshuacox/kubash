439040c5341c06829109f27e918fafd582183d46
Josh Cox <josh@webhosting.coop>
2019-07-15 18:00:02 -0500

    builder

--- .ci/builder.sh
+++ .ci/builder.sh
@@ -1,17 +1,10 @@
 #!/bin/bash -l
-ls -alh .ci/header
 . .ci/header
-echo builder.sh
-whoami
-set -eux
 . ~/.bashrc
-printenv
-which kubash
+echo builder.sh
+set -eux
 
 main () {
-  kubash build -y --target-os ubuntu1.13.8 --verbosity=100
-  kubash build -y --target-os ubuntu1.14.4 --verbosity=100
-  kubash build -y --target-os ubuntu1.15.0 --verbosity=100
   kubash build -y --target-os coreos --builder coreos 
 }
 
