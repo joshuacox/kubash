18eff3480d601c81a4c1b3f0dd0aba22391ca0a5
Josh Cox <josh@webhosting.coop>
2019-07-21 10:02:10 -0500

    verbosity

--- .ci/tester.sh
+++ .ci/tester.sh
@@ -11,12 +11,9 @@ cleanup () {
 main () {
   set -eux
   kubash -n coreos1 yaml2cluster ~/.kubash/examples/coreos1-stacked.yaml
-  kubash -n coreos1 provision --verbosity=100
-  kubash -n coreos1 init --verbosity=100
+  kubash -n coreos1 provision
+  kubash -n coreos1 init
   cp ~/.kubash/clusters/coreos1/config ~/.kube/config
-  date -I > $THIS_CLUSTER_NAME.log
-  kubectl get nodes >> coreos1.log
-  kubectl get pods --all-namespaces >> coreos1.log
   cd ~/.kubash
   bats .ci/.tests.bats
   cleanup
