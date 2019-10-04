b90414dbb6e50a2070c18485d2ff844084a46514
Josh Cox <josh@webhosting.coop>
2019-10-04 12:46:01 -0500

    allinone

--- lib/utils.bash
+++ lib/utils.bash
@@ -197,12 +197,14 @@ get_major_minor_kube_version () {
   #command2run="kubeadm version 2>/dev/null |sed \'s/^.*Major:\"\([1234567890]*\)\", Minor:\"\([1234567890]*\)\", GitVersion:.*$/\1,\2/\'"
   squawk 101 "get_major_minor_kube_version set command"
   set +e
-  command2run="kubeadm version 2>/dev/null"
-  TEST_KUBEADM_VER_STEP_1=$(sudo_command $this_port $this_user $this_host "$command2run")
-  squawk 101 "get_major_minor_kube_version step 1 output=$TEST_KUBEADM_VER_STEP_1"
-  TEST_KUBEADM_VER_STEP_2=$(echo $TEST_KUBEADM_VER_STEP_1 | grep -v -P '^#')
-  squawk 101 "get_major_minor_kube_version step 2 output=$TEST_KUBEADM_VER_STEP_2"
-  TEST_KUBEADM_VER=$(echo $TEST_KUBEADM_VER_STEP_2 | sed 's/^.*Major:\"\([1234567890]*\)\", Minor:\"\([1234567890]*\)\", GitVersion:.*$/\1,\2/' )
+  #command2run="kubeadm version 2>/dev/null"
+  command2run="kubeadm version 2>/dev/null |sed \'s/^.*Major:\"\([1234567890]*\)\", Minor:\"\([1234567890]*\)\", GitVersion:.*$/\1,\2/\'"
+  TEST_KUBEADM_VER=$(sudo_command $this_port $this_user $this_host "$command2run")
+  #TEST_KUBEADM_VER_STEP_1=$(sudo_command $this_port $this_user $this_host "$command2run")
+  #squawk 101 "get_major_minor_kube_version step 1 output=$TEST_KUBEADM_VER_STEP_1"
+  #TEST_KUBEADM_VER_STEP_2=$(echo $TEST_KUBEADM_VER_STEP_1 | grep -v -P '^#')
+  #squawk 101 "get_major_minor_kube_version step 2 output=$TEST_KUBEADM_VER_STEP_2"
+  #TEST_KUBEADM_VER=$(echo $TEST_KUBEADM_VER_STEP_2 | sed 's/^.*Major:\"\([1234567890]*\)\", Minor:\"\([1234567890]*\)\", GitVersion:.*$/\1,\2/' )
   squawk 101 "get_major_minor_kube_version step 3 output=$TEST_KUBEADM_VER"
   squawk 101 "get_major_minor_kube_version exports"
   export KUBE_MAJOR_VER=$(echo $TEST_KUBEADM_VER|cut -f1 -d,)
